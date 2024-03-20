b68c1577e30c31aa3d3995ba7632e61cfda79206
ajbuwidnnfjwj <416tommy@naver.com>
2024-03-20 22:08:15 +0900

    initial project making

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.snust.tetrij {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.almasb.fxgl.all;
+
+    opens com.snust.tetrij to javafx.fxml;
+    exports com.snust.tetrij;
+}
