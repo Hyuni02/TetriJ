dfc7219eab30e3535d40aa06d5467784b2f048c1
ajbuwidnnfjwj <416tommy@naver.com>
2024-04-26 16:41:06 +0900

    BlockTest에 rws적용

--- src/main/java/com/snust/tetrij/BlockTest.java
+++ src/main/java/com/snust/tetrij/BlockTest.java
@@ -11,25 +11,25 @@ public class BlockTest {
     @Test
     public void EasyBlockTest() {
         Map<Integer, Integer> blockCount = new HashMap<>();
-        TetrisBoardController.field = new ArrayList<Integer>();
+        ArrayList<Integer> field = new ArrayList<Integer>();
         for (int i = 0; i < 7; i++) {
             blockCount.put(i, 0);
         }
 
         int totalSelections = 1000;
         for (int i = 0; i < totalSelections; i++) {
-            TetrisBoardController.RWS(Tetris.difficulty.EASY);
+            field.add(TetrisBoardController.RWS(Tetris.difficulty.EASY));
         }
-        for (int i : TetrisBoardController.field){
+        for (int i : field){
             int selectedBlock = i;
             blockCount.put(selectedBlock, blockCount.get(selectedBlock) + 1);
         }
 
         //확률 검증 ㄱㄱ
         double expectedIRate = 0.16; // 쉬움 난이도에서 I 블록의 예상 선택 비율
-        double actualRate = blockCount.get(1) / (double) TetrisBoardController.field.size();
+        double actualRate = blockCount.get(1) / (double) field.size();
         System.out.println("EASY 모드: ");
-        System.out.println("총 블록 수: " + TetrisBoardController.field.toArray().length);
+        System.out.println("총 블록 수: " + field.toArray().length);
         System.out.println("I 개수: " + blockCount.get(1));
         System.out.println("I 블록 비율: " + actualRate);
 
@@ -40,7 +40,7 @@ public class BlockTest {
     @Test
     public void HardBlockTest() {
         Map<Integer, Integer> blockCount = new HashMap<>();
-        TetrisBoardController.field = new ArrayList<Integer>();
+        ArrayList<Integer> field = new ArrayList<Integer>();
         for (int i = 0; i < 7; i++) {
             blockCount.put(i, 0);
         }
@@ -48,18 +48,18 @@ public class BlockTest {
         // 200번 이상 블록 선택 반복 -> 블록 14400개는 만들어짐 ㅎㅎ
         int totalSelections = 1000;
         for (int i = 0; i < totalSelections; i++) {
-            TetrisBoardController.RWS(Tetris.difficulty.HARD);
+            field.add(TetrisBoardController.RWS(Tetris.difficulty.HARD));
         }
-        for (int i : TetrisBoardController.field){
+        for (int i : field){
             int selectedBlock = i;
             blockCount.put(selectedBlock, blockCount.get(selectedBlock) + 1);
         }
 
         //확률 검증 ㄱㄱ
         double expectedIRate = 0.11; // 어려움 난이도에서 I 블록의 예상 선택 비율
-        double actualRate = blockCount.get(1) / (double) TetrisBoardController.field.size();
+        double actualRate = blockCount.get(1) / (double) field.size();
         System.out.println("Hard 모드: ");
-        System.out.println("총 블록 수: " + TetrisBoardController.field.toArray().length);
+        System.out.println("총 블록 수: " + field.toArray().length);
         System.out.println("I 개수: " + blockCount.get(1));
         System.out.println("I 블록 비율: " + actualRate);
 
