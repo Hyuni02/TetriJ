1f157448e3b42582febe7e5e4c3ab1ea84a25c00
ErrorCode02 <jackjsh@naver.com>
2024-05-12 20:58:26 +0900

    4x4폭탄 문자 B->b로 변경

--- src/main/java/com/snust/tetrij/Tetris.java
+++ src/main/java/com/snust/tetrij/Tetris.java
@@ -292,7 +292,10 @@ public class Tetris extends Application {
                     if (MESH[y][x] == 'L'){
                         t.setText("L");
                     }
-                    else if(MESH[y][x] == 'b' || MESH[y][x] == 'B'){
+                    else if(MESH[y][x] == 'b'){
+                        t.setText("b");
+                    }
+                    else if(MESH[y][x] == 'B'){
                         t.setText("B");
                     }
                     else if(MESH[y][x] == 'V'){
