5c5e3e439c3b660b888d555bb93d54d593e2b34c
ajbuwidnnfjwj <416tommy@naver.com>
2024-05-21 15:30:38 +0900

    interrupt 문제 해결

---
+++ src/main/java/com/snust/tetrij/GameScene/GameSceneSingle/PlayerThreadSingle.java
@@ -0,0 +1,55 @@
+package com.snust.tetrij.GameScene.GameSceneSingle;
+import static com.snust.tetrij.GameScene.GameSceneSingle.SingleTetrisController.controller_s;
+import static com.snust.tetrij.GameScene.GameSceneSingle.SingleTetrisView.view_s;
+
+public class PlayerThreadSingle extends Thread {
+    String thread_name;
+
+    public PlayerThreadSingle(String thread_name) {
+        super(thread_name);
+        this.thread_name = thread_name;
+    }
+
+
+    @Override
+    public void run() {
+        SingleBoardController.generateTetromino();
+        SingleBoardController.generateTetromino();
+
+        int speedLevel = 0;
+        while (!controller_s.isGameOver) {
+            int freq = 300;
+            if (controller_s.isPaused)
+                continue;
+
+            int finalFreq = 0;
+            int boost = 30;
+            switch (controller_s.currentDifficulty) {
+                case EASY -> finalFreq = freq - speedLevel * (int) (boost * 0.8f);
+                case HARD -> finalFreq = freq - speedLevel * (int) (boost * 1.2f);
+                default -> finalFreq = freq - speedLevel * boost; //normal or item
+            }
+            view_s.color_mesh();
+            try {
+                this.sleep(finalFreq);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            SingleBoardController.softDrop(SingleBoardController.bag.get(0)); //한칸 드랍
+            if (speedLevel == 0)
+                controller_s.score++;
+            else if (speedLevel == 1)
+                controller_s.score += 2;
+            else if (speedLevel == 2)
+                controller_s.score += 3;
+            view_s.scoreText.setText("Score: " + Integer.toString(controller_s.score));
+            view_s.level.setText("Lines: " + Integer.toString(controller_s.linesNo));
+
+            //게임오바
+            if (controller_s.top > 19) {
+                break;
+            }
+        }
+    }
+}
--- src/main/java/com/snust/tetrij/GameScene/GameSceneSingle/SingleTetrisController.java
+++ src/main/java/com/snust/tetrij/GameScene/GameSceneSingle/SingleTetrisController.java
@@ -18,59 +18,9 @@ public class SingleTetrisController extends GameControllerBase {
         view_s.stage = stage;
         currentDifficulty = difficulty;
 
-        Thread thread = null;
-        Thread finalThread = thread;
-
         SingleKeyController.addListenerGameControl(view_s.scene);
 
-        Runnable task = new Runnable() {
-            public void run() {
-                SingleBoardController.generateTetromino();
-                SingleBoardController.generateTetromino();
-                view_s.color_mesh();
-
-                int speedLevel = 0;
-                while (!isGameOver) {
-                    int freq = 300;
-                    try {
-                        //일시정지
-                        if (isPaused)
-                            continue;
-
-                        int finalFreq = 0;
-                        int boost = 30;
-                        switch (controller_s.currentDifficulty) {
-                            case EASY -> finalFreq = freq - speedLevel * (int) (boost * 0.8f);
-                            case HARD -> finalFreq = freq - speedLevel * (int) (boost * 1.2f);
-                            default -> finalFreq = freq - speedLevel * boost; //normal or item
-                        }
-                        Thread.sleep(finalFreq);
-
-                        if (speedLevel == 0)
-                            score++;
-                        else if (speedLevel == 1)
-                            score += 2;
-                        else if (speedLevel == 2)
-                            score += 3;
-
-                        view_s.scoreText.setText("Score: " + Integer.toString(score));
-                        view_s.level.setText("Lines: " + Integer.toString(linesNo));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    SingleBoardController.softDrop(SingleBoardController.bag.get(0)); //한칸 드랍
-                    view_s.color_mesh(); //색칠
-
-                    //게임오바
-                    if (controller_s.top > 19) {
-                        finalThread.interrupt();
-                    }
-                }
-                // GameOver(stage, dif);
-            }
-        };
-        thread = new Thread(task);
-        thread.start();
+        PlayerThreadSingle playerThread = new PlayerThreadSingle("Single Play");
+        playerThread.start();
     }
 }
