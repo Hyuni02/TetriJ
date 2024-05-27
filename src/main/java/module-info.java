7881f655ccee13d70f311892f9e78b498492c0d9
HYOJEONG0213 <fredflem@naver.com>
2024-05-28 01:30:28 +0900

    scope 안 없앤 버전

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,17 @@ module com.snust.tetrij {
     requires org.json;
     requires junit;
     requires jdk.xml.dom;
+    requires reflections;
+    requires org.junit.jupiter.api;
+    requires org.junit.platform.commons;
+    requires org.junit.platform.engine;
+    requires org.junit.platform.launcher;
+    requires com.google.common;
+    requires org.junit.jupiter.engine;
+    exports com.snust.tetrij to reflections, org.junit.jupiter.api, org.junit.platform.commons, junit;
 
 
     opens com.snust.tetrij to javafx.fxml;
-    exports com.snust.tetrij;
     exports com.snust.tetrij.tetromino;
     opens com.snust.tetrij.tetromino to javafx.fxml;
     exports com.snust.tetrij.GameScene;
@@ -20,5 +27,7 @@ module com.snust.tetrij {
     opens com.snust.tetrij.Controller to javafx.fxml;
     exports com.snust.tetrij.GameScene.GameSceneSingle;
     opens com.snust.tetrij.GameScene.GameSceneSingle to javafx.fxml;
+//    exports com.snust.tetrij.UnitTest;
+
 
 }
