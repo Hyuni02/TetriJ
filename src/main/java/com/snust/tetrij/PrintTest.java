b8e7d4f3b0e888e7730c9e63cfaec2725555d23b
HYOJEONG0213 <fredflem@naver.com>
2024-05-30 00:46:07 +0900

    양효정 - 작고 소중한 AspectJ 실험
    pow.xml랑 aop.xml 너무 더러워서 아직 안올림 ㅎㅎ
    처음으로 테스트 성공한 파일 백업용!!

---
+++ src/main/java/com/snust/tetrij/PrintTest.java
@@ -0,0 +1,12 @@
+package com.snust.tetrij;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class PrintTest {
+
+    @Before("execution(* com.snust.tetrij..*(..))")
+    public void beforeMethod() {
+        System.out.println("Before method execution");
+    }
+}
